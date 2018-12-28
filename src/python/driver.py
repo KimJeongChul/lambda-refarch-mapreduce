bdbe989a308ff2bcd876fb27cd67ec70f9f2c867
KimJungChul <kjc5443@gmail.com>
2018-12-28 14:57:48 +0900

    python file encoding

--- src/python/driver.py
+++ src/python/driver.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 '''
  Driver to start BigLambda Job
  
--- src/python/lambdautils.py
+++ src/python/lambdautils.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 '''
  * Copyright 2016, Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
